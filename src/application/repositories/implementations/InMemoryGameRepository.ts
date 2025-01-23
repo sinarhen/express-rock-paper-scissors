9309b34cf5ebaa9ca1853988d82a754496a26eba
sinarhen <nicklchannel8@gmail.com>
2025-01-23 02:08:20 +0200

    Add InMemoryGameRepository implementation and utility for generating six-digit codes

---
+++ src/application/repositories/implementations/InMemoryGameRepository.ts
@@ -0,0 +1,38 @@
+// src/infrastructure/repositories/InMemoryGameRepository.ts
+
+import Game from "@/domain/entities/Game"
+import { IGameRepository } from "../interfaces/IGameRepository"
+import { db } from "@/infrastructure/database"
+
+export class InMemoryGameRepository implements IGameRepository {
+  public findByCode(code: string): Game | null {
+    return db.games[code] || null
+  }
+
+  public create(game: Game): Game {
+    if (db.games[game.code]) {
+      throw new Error(`Game with code ${game.code} already exists`)
+    }
+    db.games[game.code] = game
+    return game
+  }
+
+  public update(game: Game): Game {
+    if (!db.games[game.code]) {
+      throw new Error(`Game with code ${game.code} not found`)
+    }
+    db.games[game.code] = game
+    return game
+  }
+
+  public delete(gameId: string): void {
+    if (!db.games[gameId]) {
+      throw new Error(`Game with ID ${gameId} not found`)
+    }
+    delete db.games[gameId]
+  }
+
+  public listAll(): Game[] {
+    return Object.values(db.games)
+  }
+}
---
+++ src/application/utils/sixDigitCode.ts
@@ -0,0 +1,2 @@
+const generateSixCharCode = () => Math.random().toString(36).substring(8, 2)
+export default generateSixCharCode
